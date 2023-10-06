c21054ec1e6b1bdaa2f2dffa77a822ff782d7985
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-06 15:59:55 +0200

    fix: lint

--- src/app/listings/[listing]/page.tsx
+++ src/app/listings/[listing]/page.tsx
@@ -1,23 +1,21 @@
 'use client';
 import DetailPageLayout from '@/components/DetailPageLayout';
-import PageLayout from '@/components/PageLayout';
-import { Place, places } from '@/utils/mocks/places';
 import { useMapStore } from '@/utils/store/mapStore';
-import React, { useState } from 'react';
-
-
+import React from 'react';
 
 //write logic for error - should display something went wrong and back button (see figma)
 
 const ListingPage: React.FC = () => {
   const { places } = useMapStore();
   const generateStaticParams = () => {
-  return places.map((place) => ({
-    slug: place.features[0].properties.name.replace(/\s+/g, '-').toLowerCase(),
-    headline: place.features[0].properties.name,
-    //here comes te txt
-  }));
-}
+    return places.map((place) => ({
+      slug: place.features[0].properties.name
+        .replace(/\s+/g, '-')
+        .toLowerCase(),
+      headline: place.features[0].properties.name,
+      //here comes te txt
+    }));
+  };
   const place = generateStaticParams()[0];
   return (
     <DetailPageLayout
