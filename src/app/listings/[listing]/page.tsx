89b67f21c11d18d548134c53cf50f61a6dae4708
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-06 13:35:33 +0200

    fix: typescript errors fixed

--- src/app/listings/[listing]/page.tsx
+++ src/app/listings/[listing]/page.tsx
@@ -5,18 +5,19 @@ import { Place, places } from '@/utils/mocks/places';
 import { useMapStore } from '@/utils/store/mapStore';
 import React, { useState } from 'react';
 
-export function generateStaticParams() {
+
+
+//write logic for error - should display something went wrong and back button (see figma)
+
+const ListingPage: React.FC = () => {
   const { places } = useMapStore();
+  const generateStaticParams = () => {
   return places.map((place) => ({
     slug: place.features[0].properties.name.replace(/\s+/g, '-').toLowerCase(),
     headline: place.features[0].properties.name,
     //here comes te txt
   }));
 }
-
-//write logic for error - should display something went wrong and back button (see figma)
-
-const ListingPage: React.FC = () => {
   const place = generateStaticParams()[0];
   return (
     <DetailPageLayout
