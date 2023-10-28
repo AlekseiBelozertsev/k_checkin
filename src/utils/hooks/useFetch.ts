35dcf80d9339916534a41138ccfc5127a35e8054
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-28 20:52:14 +0200

    feat: useFetch hook created

---
+++ src/utils/hooks/useFetch.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { Place } from "../store/mapStore";
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState<Place[]>();
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, [url]);
+  return data;
+}
+
