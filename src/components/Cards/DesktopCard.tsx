9d878cde5ce15f70fa34dc1d6c28a5f5987db78e
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-10-30 16:26:17 +0100

    fix: card updated

--- src/components/Cards/DesktopCard.tsx
+++ src/components/Cards/DesktopCard.tsx
@@ -2,21 +2,30 @@ import { MenuElementProps } from '@/utils/menu';
 import menuElementStyles from './menuElement.module.scss';
 import React from 'react';
 import Icon from '../Icon';
-import { useMapStore } from '@/utils/store/mapStore';
 import Button from '../Button';
+import { useFetch } from '@/utils/hooks/useGetData';
+import Loader from '../Loader';
 
 const DesktopCard: React.FC<MenuElementProps> = ({ icon, name }) => {
-  const places = useMapStore((store) => store.places);
+  const data = useFetch(`${process.env.NEXT_PUBLIC_LOCALHOST}/getPlaces`);
+
   return (
     <div className={menuElementStyles.mainDesktop}>
       <div className={menuElementStyles.contentWrapper}>
         <div className={menuElementStyles.iconWrapper}>
           <Icon width={80} height={80} src={icon} />
         </div>
-        <span
-          className={menuElementStyles.elementName}
-        >{`${name} (${places.length})`}</span>
-        <Button text={`Open listings`} type={`primary`} onClick={() => null} />
+        <div className={menuElementStyles.textWrapper}>
+          <span className={menuElementStyles.elementName}>{`${name}`}</span>
+          {data ? (
+            <span
+              className={menuElementStyles.elementName}
+            >{`(${data.length})`}</span>
+          ) : (
+            <Loader />
+          )}
+        </div>
+        <Button text={`Open listings`} type={`terciary`} onClick={() => null} />
       </div>
     </div>
   );
