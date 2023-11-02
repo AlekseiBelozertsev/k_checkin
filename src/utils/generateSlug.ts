e995b19c4dbeef4e30ea4b69930b1cb619a8b31a
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-11-02 10:35:51 +0100

    feat: slug generator

---
+++ src/utils/generateSlug.ts
@@ -0,0 +1,8 @@
+export const generateSlug = (input: string) => {
+  const slug = input
+  .normalize('NFD')
+  .replace(/[\u0300-\u036f]/g, '')
+  .replace(/\s+/g, '-')
+  .toLowerCase();
+  return slug;
+}
