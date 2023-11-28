53501423e8f651b2107607c9a32935b88936c20e
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-11-28 10:39:31 +0100

    chore: unused stores deleted

--- src/utils/store/dataStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-import { Place } from './mapStore';
-
-type DataStoreType = {
-  places: Place[];
-  postData: (url: string, data: Place) => void;
-  getData: (url: string) => void;
-};
