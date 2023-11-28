36ed53c3cbcafcfaa4e44de24afffe499e5be019
alexeibelozertsev <belozertsev.alexei@gmail.com>
2023-11-28 10:38:53 +0100

    chore: map store cleaned from places logic

--- src/utils/store/mapStore.ts
+++ src/utils/store/mapStore.ts
@@ -1,18 +1,10 @@
 import { create } from 'zustand';
+import { Place, usePlaceStore } from './placeStore';
 
 export interface MapFiltersType {
   name: string;
   isActive: boolean;
 }
-
-export type Place = {
-  id: string;
-  name: string;
-  country: string;
-  address: string;
-  coordinates: [number, number];
-  createdAt: string;
-};
 
 export const mapFilters: MapFiltersType[] = [
   {
@@ -38,23 +30,15 @@ export const mapFilters: MapFiltersType[] = [
 ];
 
 interface MapStoreType {
-  places: Place[];
-  setPlaces: (places: Place[]) => void;
-  addPlace: (place: Place) => void;
   currentCenter: [number, number];
   setCurrentCenter: (place: Place) => void;
   zoom: number;
   setZoom: (zoom: number) => void;
   isMapLoaded: boolean | undefined;
   onMapLoad: () => void;
-  postData: (url: string, data: Place) => void;
-  getData: (url: string) => void;
 }
 
 export const useMapStore = create<MapStoreType>((set, get) => ({
-  places: [],
-  setPlaces: (places) => set(() => ({ places: places })),
-  addPlace: (place) => set((state) => ({ places: [...state.places, place] })),
   currentCenter: [14.41854, 50.073658],
   setCurrentCenter: (place) =>
     set({ currentCenter: [place.coordinates[1], place.coordinates[0]] }),
@@ -62,32 +46,4 @@ export const useMapStore = create<MapStoreType>((set, get) => ({
   setZoom: (zoom) => set(() => ({ zoom: zoom })),
   isMapLoaded: false,
   onMapLoad: () => set({ isMapLoaded: true }),
-  postData: async (url, data) => {
-    const stringifiedData = JSON.stringify(get().places);
-    localStorage.setItem('places', stringifiedData);
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    }).catch((err) => {
-      console.log(`${err} - Data will be stored in localStorage`);
-    });
-  },
-  getData: async (url) => {
-    const localStorageData = localStorage.getItem('places');
-    if (localStorageData) {
-      const localStorageDataParsed = JSON.parse(localStorageData);
-      set(() => ({ places: localStorageDataParsed }));
-    }
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        set(() => ({ places: data }));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
 }));
